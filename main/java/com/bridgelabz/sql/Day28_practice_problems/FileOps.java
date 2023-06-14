cceb63c962fc322df344b9ec54250d9915d06626
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-14 18:57:42 +0530

    FileOps.java

--- main/java/com/bridgelabz/sql/Day28_practice_problems/FileOps.java
+++ main/java/com/bridgelabz/sql/Day28_practice_problems/FileOps.java
@@ -3,7 +3,8 @@ package com.bridgelabz.sql.Day28_practice_problems;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class FileOps {
+public class FileOps 
+{
 	 void writeDataToDestination(HashMap<String, AddressBook> addressBookHashMap) throws IOException {} 
 	 
 	
